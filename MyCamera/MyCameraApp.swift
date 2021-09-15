b75cfd0868196215a5967a78e26477ccb998da1e
zhangjingjian <zhangjianjian@joyy.sg>
2021-09-15 17:55:03 +0800

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by zjj on 2021/9/15.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
